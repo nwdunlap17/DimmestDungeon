7f50a3049a24fc956db7f640b8108eb9edaf891b
github username <github email address>
2019-07-29 16:05:57 -0700

    changesto combatmanager

--- lib/CombatManager.rb
+++ lib/CombatManager.rb
@@ -3,24 +3,34 @@ class CombatManager
     def initialize(heroes_array,monsters_array)
         #index 1-2 are front, 3-4 back
         #each array position should contain an object instance of representative thing
+    @combat_is_over = false
+    @heroes_alive = true
+    @monsters_alive = true
     @heroes_position = heroes_array
     @monsters_position = monsters_array
     heroes_aggro = []
     end
 
     def battle_sequence
-        heroes = heroes_position
-        monsters = monsters_position
-        battle_array = heroes.concat(monsters)
-        if 
-            battle_array.sample
-
-
+        while @combat_is_over == false
+            if battle_array.empty? == true
+                heroes = heroes_position
+                monsters = monsters_position
+                battle_array = heroes.concat(monsters)
+            end
+            character_picked = battle_array.sample
+            #some course of action 
+            battle_array.delete(character_picked)
+            self.check_for_dead
+        end
+        if heroes_alive == false
+            #end game method
+        elsif monsters_alive == false
+            #YEET we won
+        end
     end
 
-    def turn_order?
-        
-        
+    def add_aggro
     end
 
     def apply_damage(damaged_object,damage_dealt)
@@ -36,19 +46,22 @@ class CombatManager
             if hero.alive? == false
                 heroes_position.delete(hero)
                 heroes_aggro.delete(hero)
+                if heroes_position.empty? == true
+                    @heroes_alive = false
+                    @combat_is_over == true
+                end
             end
         end
         self.monsters_position.each do |monster|
             if monster.alive? == false
                 monsters_position.delete(monster)
+                if monsters_position.empty? == true
+                    @monsters_alive = false
+                    @combat_is_over == true
+                end
             end
         end
     end
-            
-
-        
-        
-
     
 
         #invisibility pulls one aggro
