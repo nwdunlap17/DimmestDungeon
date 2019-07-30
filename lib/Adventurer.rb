c6b454eba78ce3cd252b78f1c6b0bfde397dca55
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-30 12:27:48 -0700

    Skills are made, display update, combatant now module

--- lib/Adventurer.rb
+++ lib/Adventurer.rb
@@ -1,3 +1,17 @@
-class Adventurer < Combatant
-    attr_accessor :name
+class Adventurer
+    attr_accessor :name, :current_MP, :max_MP, :job
+
+    include Combatant
+    def initialize(attack=4,defense=0,max_HP=10)
+        super(attack,defense,max_HP)
+        @name = "Hero"
+        @job = "none"
+        @max_MP = 0
+        @current_MP = 0
+    end
+
+    def generate_new_adventurer_with_job
+        
+    end
+
 end
