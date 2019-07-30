8cd2c68776cdda39fd34342e6e09d1770f08f780
github username <github email address>
2019-07-30 14:21:20 -0700

    Treasure/rooms

--- lib/Monster.rb
+++ lib/Monster.rb
@@ -1,7 +1,17 @@
-class Monster < Combatant
+class Monster < ActiveRecord::Base
+    include Combatant
     attr_accessor :name
 
     def initialize
         super
+        @monster_description = ""
+    end
+
+    def self.load_monsters
+        api_response = RestClient.get("https://api.hearthstonejson.com/v1/25770/enUS/cards.json")
+        monster_json = JSON.parse(api_response.body)
+        new_monster = monster_json.select do |monster_hash|
+            monster_hash[:type] == "MINION"
+        end
     end
 end
