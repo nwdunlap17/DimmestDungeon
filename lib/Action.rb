215d6af988b99460a3a82b1eb26274bbfd983eda
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-29 18:01:13 -0700

    somestuff

--- lib/Action.rb
+++ lib/Action.rb
@@ -1,12 +1,13 @@
 class Action
-    attr_accessor :selection_type, :target_self
-    attr_accessor :Damage_Multiplier, :atk_buff, :def_buff, :heal_value, :aggro_change
+    attr_accessor :selection_type, :target_self, :mp_cost, :action_name
+    attr_accessor :damage_Multiplier, :atk_buff, :def_buff, :heal_value, :aggro_change
     #selection_type = "1 Enemy", "All Enemies", "1 Ally", "All allies"
     #target self
 
     def initialize
         @selection_type = "none"
-        @Damage_Multiplier = 0
+        @damage_Multiplier = 0
+        @mp_cost = 0
         @target_self = false
         @atk_buff = 0
         @def_buff = 0
@@ -16,13 +17,25 @@ class Action
 
     def Action.basic_attack
         skill = Action.new
+        skill.action_name = "Attack"
         skill.selection_type = "1 Enemy"
-        skill.Damage_Multiplier = 1
+        skill.damage_Multiplier = 1
         skill.target_self = false
         skill.atk_buff = 0
         skill.def_buff = 0
+        skill.mp_cost = 0
         skill.heal_value = 0
         skill.aggro_change = 0
         return skill
+    end
+
+    def Action.make_attack(name,targets_all,damage_Multiplier)
+        skill = Action.new
+        if targets_all
+            skill.selection_type = "All Enemies"
+        else
+            skill.selection_type = "1 Enemy"
+        end
+        skill.damage_Multiplier = damage_Multiplier
     end
 end
