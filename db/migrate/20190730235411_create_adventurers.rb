ead60dfa35a8a0c722e72373be9683f687b20b91
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-31 10:41:39 -0700

    Action Schema

--- db/migrate/20190730235411_create_adventurers.rb
+++ db/migrate/20190730235411_create_adventurers.rb
@@ -7,6 +7,8 @@ class CreateAdventurers < ActiveRecord::Migration[5.2]
       a.integer :defense
       a.integer :max_HP
       a.integer :max_MP
+      a.integer :skill1_id
+      a.integer :skill2_id
       a.timestamps
     end
   end
