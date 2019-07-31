19c520e535a56fd3894abca30d66923761fd27ed
github username <github email address>
2019-07-31 11:09:51 -0700

    modified room and monster functions

--- lib/Party.rb
+++ lib/Party.rb
@@ -1,6 +1,7 @@
 class Party
-    def initialize()
-        heroes_array = [nil,nil,nil,nil]
+    attr_accessor :heroes_array
+    def initialize
+        @heroes_array = [nil,nil,nil,nil]
     end
 
     def add_to_party
