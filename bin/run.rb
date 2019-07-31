246a92ba6b1755c0bcdb9b7ee91fc3d91192f6c4
Nick Dunlap <nwdunlap17@gmail.com>
2019-07-31 15:33:13 -0700

    actually works

--- bin/run.rb
+++ bin/run.rb
@@ -4,14 +4,10 @@ require_relative('environment.rb')
 # Action.Load_Action_List
 # Monster.load_monsters
 
-#banana = Room.new(10)
-slime = Monster.manual_generation("Slime",4,0,20)
-boblin = Monster.manual_generation("Boblin",2,2,20)
+banana = Room.new(10)
+# slime = Monster.manual_generation("Slime",4,0,20)
+# boblin = Monster.manual_generation("Boblin",2,2,20)
 
-monsters = [slime,boblin]
+# monsters = [slime,boblin]
 
-nick = Adventurer.manual_generation("Nick",2,4,10)
-katana = Adventurer.manual_generation("Katana",10,10,20)
-
-heros = [nick,katana]
-CombatManager.new(heros,monsters)
+# CombatManager.new(Party.new.heroes_array,monsters)
